4f96eb4d6156704725c927cf4b9799d69600b509
ArmanPefourque <armanpefo@gmail.com>
2024-11-23 20:02:19 -0800

    tfds

--- COUNTERSPELL HACKATHON GAME/Assets/Scripts/EnemyScript.cs
+++ COUNTERSPELL HACKATHON GAME/Assets/Scripts/EnemyScript.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
 using Pathfinding;
 using System.Collections.Generic;
+using System.IO;
 
 public class EnemyScript : MonoBehaviour
 {
+    public GameObject sm;
     public GameObject[] floors;
     public string first = "hi";
     private AIPath path;
@@ -11,39 +13,45 @@ public class EnemyScript : MonoBehaviour
     [SerializeField] private float moveSpeed;
     [SerializeField] private GameObject target;
     public List<GameObject> flors = new List<GameObject>();
+    public SanityScript ss;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        sm = GameObject.FindGameObjectWithTag("health bar");
+        ss = sm.GetComponent<SanityScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
-               if (first == "hi"){
-            first = "bye";
-        floors = GameObject.FindGameObjectsWithTag("floor");
-        
-        target = GameObject.FindGameObjectWithTag("Player");
-        path = GetComponent<AIPath>();
-
-       foreach (GameObject floor  in floors)
+        if (first == "hi")
         {
-            float xdist = floor.transform.position.x - target.transform.position.x;
-            float ydist = floor.transform.position.y - target.transform.position.y;
-            if (Mathf.Sqrt((xdist*xdist) +(ydist*ydist)) > 10) {
-                flors.Add(floor);
-            }
-        }
+            first = "bye";
+            floors = GameObject.FindGameObjectsWithTag("floor");
 
-        transform.position = flors[Random.Range(0, floors.Length)].transform.position;
+            target = GameObject.FindGameObjectWithTag("Player");
+            path = GetComponent<AIPath>();
+
+            foreach (GameObject floor in floors)
+            {
+                float xdist = floor.transform.position.x - target.transform.position.x;
+                float ydist = floor.transform.position.y - target.transform.position.y;
+                if (Mathf.Sqrt((xdist * xdist) + (ydist * ydist)) > 10)
+                {
+                    flors.Add(floor);
+                }
+            }
+
+            transform.position = flors[Random.Range(0, floors.Length)].transform.position;
         }
         path.maxSpeed = moveSpeed;
         path.destination = target.transform.position;
+        if (health == 0)
+        {
 
-        if (health <= 0){
             Destroy(gameObject);
+            ss.AddTrauma(1);
         }
-        
+
     }
 }
